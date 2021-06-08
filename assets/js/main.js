5439d7b835d211d98eb34198572e542457c049a7
Le Minh Tri <minhtri0795@gmail.com>
2021-06-08 20:52:27 +0700

    GSAP test animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-    
+    gsap.to(".header-video-container video", {
+        scrollTrigger: {
+            trigger: ".header-video-container video",
+            start: "top 90%",
+            end: "center center",
+            scrub: true,
+        },
+        scale: 1,
+        ease:"power1.in"
+    })
+    gsap.to(".home-header-wrapper .text-wrapper", {  
+        scrollTrigger: {
+            trigger: ".home-header-wrapper .text-wrapper",
+            start: "top 33%",
+            end: "bottom top",
+            markers:true,
+            scrub: 1
+        },
+        y:600,
+        ease:"power1.in"
+    })
 });
